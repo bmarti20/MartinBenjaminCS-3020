3427be40590ed3bd3966d2f7e5fe9a23ea673f94
Benjamin Martin <bmartin6245@yahoo.com>
2019-01-30 16:12:58 -0700

    Test commit to push to correct repository

---
+++ Homework 1/Homework 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Welcome to Program 1! *****");
+            Console.WriteLine("Please select an option: ");
+
+            Console.ReadKey();
+        }
+    }
+}
