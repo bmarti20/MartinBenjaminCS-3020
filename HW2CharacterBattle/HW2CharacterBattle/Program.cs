d3b3571349e6af177984473c3bd22b7be1e8c632
Benjamin Martin <bmartin6245@yahoo.com>
2019-02-08 14:24:08 -0700

    Created Project and abstract class + 3 role classes

---
+++ HW2CharacterBattle/HW2CharacterBattle/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace HW2CharacterBattle
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public abstract class Character
+    {
+        protected string name;
+        public abstract void Attack();
+    }
+
+    public class Warrior : Character
+    {
+        public Warrior(string name)
+        {
+            this.name = name;
+        }
+
+        public override void Attack()
+        {
+
+        }
+    }
+
+    public class Archer : Character
+    {
+        public Archer(string name)
+        {
+            this.name = name;
+        }
+
+        public override void Attack()
+        {
+
+        }
+    }
+
+    public class Mage : Character
+    {
+        public Mage(string name)
+        {
+            this.name = name;
+        }
+
+        public override void Attack()
+        {
+
+        }
+    }
+}
