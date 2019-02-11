3145aec0372376791f30a0b11277b483bf35ce31
Benjamin Martin <bmartin6245@yahoo.com>
2019-02-10 20:36:19 -0700

    minor changes

--- HW2CharacterBattle/HW2CharacterBattle/Program.cs
+++ HW2CharacterBattle/HW2CharacterBattle/Program.cs
@@ -6,9 +6,12 @@ namespace HW2CharacterBattle
 {
     public class Program
     {
+        static Iplayerclass p1;
+
         static void Main(string[] args)
         {
             string p1name, p2name;
+
             Console.WriteLine("***** Welcome to Medieval Combat! *****");
             Console.WriteLine("Player 1, enter your name: ");
             p1name = Console.ReadLine();
@@ -28,16 +31,26 @@ namespace HW2CharacterBattle
                 Console.WriteLine("Invalid input. Please enter a number between 1 and 3.");
                 goto p1choose;
             }
-            switch (choice)             // Creates an instance of a class with p1's name
-            {
-                case 1: { Warrior p1 = new Warrior(p1name); break; }
-                case 2: { Archer p1 = new Archer(p1name); break; }
-                case 3: { Mage p1 = new Mage(p1name); break; }
-                default:
-                    Console.WriteLine("Invalid input. Please enter a number between 1 and 3.");
-                    goto p1choose;
-            }
 
+            
+
+            if (choice == 1)
+                p1 = new Warrior(p1name);
+
+            Console.WriteLine(p1.name.ToString());
+            
+
+            //switch (choice)             // Creates an instance of a class with p1's name
+            //{
+            //    case 1: Warrior p1 = new Warrior(p1name); break; 
+            //    case 2: Archer p1 = new Archer(p1name); break; 
+            //    case 3: Mage p1 = new Mage(p1name); break; 
+            //    default:
+            //        Console.WriteLine("Invalid input. Please enter a number between 1 and 3.");
+            //        goto p1choose;
+            //}
+            
+            
          p2choose:
             Console.Write("{0}, choose your class: ", p2name);
             if (!int.TryParse(Console.ReadLine(), out choice)) // Checks for invalid input
@@ -45,35 +58,46 @@ namespace HW2CharacterBattle
                 Console.WriteLine("Invalid input. Please enter a number between 1 and 3.");
                 goto p2choose;
             }
-            switch (choice)             // Creates an instance of a class with p2's name
-            {
-                case 1: { Warrior p2 = new Warrior(p1name); break; }
-                case 2: { Archer p2 = new Archer(p1name); break; }
-                case 3: { Mage p2 = new Mage(p1name); break; }
-                default:
-                    Console.WriteLine("Invalid input. Please enter a number between 1 and 3.");
-                    goto p2choose;
-            }
+            //switch (choice)             // Creates an instance of a class with p2's name
+            //{
+            //    case 1: { Warrior p2 = new Warrior(p1name); break; }
+            //    case 2: { Archer p2 = new Archer(p1name); break; }
+            //    case 3: { Mage p2 = new Mage(p1name); break; }
+            //    default:
+            //        Console.WriteLine("Invalid input. Please enter a number between 1 and 3.");
+            //        goto p2choose;
+            //}
             
             // Normally I would put these into a method and call that for p1 and p2, but since I need the p1 and p2 objects to be 
             // unique, I can't think of a better way to do it :/ sorry for the ugly code!
+
+            
 
             Console.ReadKey();
         }
     }
 
-    public abstract class Character
+    public abstract class Character : Iplayerclass
     {
-        public string name;
+        //public string name;
         public string spellname;
         public int health;
         public int ap;
         public int speed;
         public char Class;
+
+        private string _name;
+        public string name { get => _name; set => _name = value; }
+
         public abstract void Attack(Character enemy);
         public abstract void SpAttack(Character enemy);
         public abstract void TakeDamage(Character enemy);
         public abstract void TakeSpDamage(Character enemy);
     }
+
+    public interface Iplayerclass
+    {
+        string name { get; set; }
+    };
 
 }
