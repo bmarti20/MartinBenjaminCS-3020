d4327cb8b72df38c55dcea9c1c6fdb60e169e40a
Benjamin Martin <bmartin6245@yahoo.com>
2019-02-17 23:27:04 -0700

    HW3: File IO project created, some minor testing done with pathnames.

---
+++ HW3FileIOandRecursion/HW3FileIOandRecursion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace HW3FileIOandRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filetype;
+            string path = AppDomain.CurrentDomain.BaseDirectory + @"Pictures";
+            Console.Write("Please enter what file type you would like to search for: ");
+            filetype = Console.ReadLine();
+            path += @"\*." + filetype;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                    Console.WriteLine(line);
+            }
+            Console.ReadKey();
+        }
+    }
+}
