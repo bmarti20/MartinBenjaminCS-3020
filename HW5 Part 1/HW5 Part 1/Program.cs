bbc49258d19f89b8165a4b713dab1cb7a4daf5d3
Benjamin Martin <bmartin6245@yahoo.com>
2019-02-27 18:41:59 -0700

    Program now sorts lists as well as separating them by color

--- HW5 Part 1/HW5 Part 1/Program.cs
+++ HW5 Part 1/HW5 Part 1/Program.cs
@@ -10,15 +10,12 @@ namespace HW5_Part_1
         static Property oriental = new Property("Blue", 100);
         static Property vermont = new Property("Blue", 120);
         static Property connecticut = new Property("Blue", 140);
-
         static Property stjames = new Property("Yellow", 180);
         static Property tennessee = new Property("Yellow", 200);
         static Property newyork = new Property("Yellow", 220);
-
         static Property atlantic = new Property("Red", 260);
         static Property ventnor = new Property("Red", 280);
         static Property marvin = new Property("Red", 300);
-
         static Property pacific = new Property("Green", 340);
         static Property carolina = new Property("Green", 360);
         static Property pennsylvania = new Property("Green", 380);
@@ -26,17 +23,13 @@ namespace HW5_Part_1
         static void Main(string[] args)
         {
             List<Property> props = new List<Property>{ ventnor, oriental, stjames, vermont, tennessee, atlantic, carolina, pennsylvania, connecticut, marvin, pacific, newyork };
-            Console.WriteLine("Hello World!");
-            Console.WriteLine(props[0].color);
             Console.WriteLine("You have a list of Property objects that all have 1 of 4 colors and a rent amount.");
             Console.WriteLine("Separate the list into 4 different lists based on color, then sort each list by increasing rent amount.");
 
             // Bad Way (using foreach loops and ifs)
             BadWay(props);
-
             // Good Way (using LINQ)
             GoodWay(props);
-
             Console.ReadKey();
         }
 
@@ -48,6 +41,7 @@ namespace HW5_Part_1
                 if (prop.color == "Red")
                     reds.Add(prop);
             }
+            reds.Sort();
 
             List<Property> blues = new List<Property>();
             foreach (Property prop in props)
@@ -55,13 +49,15 @@ namespace HW5_Part_1
                 if (prop.color == "Blue")
                     blues.Add(prop);
             }
+            blues.Sort();
 
             List<Property> yellows = new List<Property>();
             foreach (Property prop in props)
-            {
+            { 
                 if (prop.color == "Yellow")
                     yellows.Add(prop);
             }
+            yellows.Sort();
 
             List<Property> greens = new List<Property>();
             foreach (Property prop in props)
@@ -69,14 +65,28 @@ namespace HW5_Part_1
                 if (prop.color == "Green")
                     greens.Add(prop);
             }
+            greens.Sort();
         }
 
         static void GoodWay(List<Property> props)
         {
-            List<Property> reds = props.Where(t => t.color == "Red").ToList();
-            List<Property> blues = props.Where(t => t.color == "Blue").ToList();
-            List<Property> yellows = props.Where(t => t.color == "Yellow").ToList();
-            List<Property> greens = props.Where(t => t.color == "Green").ToList();
+            List<Property> reds = props.Where(t => t.color == "Red").OrderBy(t => t.rent).ToList();
+            List<Property> blues = props.Where(t => t.color == "Blue").OrderBy(t => t.rent).ToList();
+            List<Property> yellows = props.Where(t => t.color == "Yellow").OrderBy(t => t.rent).ToList();
+            List<Property> greens = props.Where(t => t.color == "Green").OrderBy(t => t.rent).ToList();
+        }
+
+        // Functions made for testing purposes
+        static void PrintColors(List<Property> props)
+        {
+            foreach (Property prop in props)
+                Console.WriteLine(prop.color);
+        }
+
+        static void PrintRent(List<Property> props)
+        {
+            foreach (Property prop in props)
+                Console.WriteLine(prop.rent);
         }
     }
 }
--- HW5 Part 1/HW5 Part 1/Property.cs
+++ HW5 Part 1/HW5 Part 1/Property.cs
@@ -4,15 +4,19 @@ using System.Text;
 
 namespace HW5_Part_1
 {
-    class Property
+    class Property : IComparable<Property>
     {
         public string color;
         public int rent;
-
         public Property(string color, int rent)
         {
             this.color = color;
             this.rent = rent;
+        }
+
+        public int CompareTo(Property prop)
+        {
+            return rent.CompareTo(prop.rent);
         }
     }
 }
